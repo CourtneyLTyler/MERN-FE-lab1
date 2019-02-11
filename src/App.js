f7f968bf96c744fdc22016fc4a08d936aebdcd7f
Courtney Tyler <courtneytyler7@gmail.com>
2019-02-11 18:52:30 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Note from './Note.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        < Note />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Note.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+    render() {
+        return (
+            <div>
+                "Hello I'm Note 1"
+            </div>
+        );
+    }
+}
+
+export default Note;
