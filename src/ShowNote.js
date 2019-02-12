f8097abffefd46caac993c71628ad7b508fd9841
Courtney Tyler <courtneytyler7@gmail.com>
2019-02-12 10:35:44 -0500

    install react router

---
+++ src/ShowNote.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowNote extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ShowNote;
---
+++ src/UpdateNotes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UpdateNotes extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UpdateNotes;
