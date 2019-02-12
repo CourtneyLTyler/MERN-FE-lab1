5577d4541cd2c173519c43ac2671e12ca1e5653a
Courtney Tyler <courtneytyler7@gmail.com>
2019-02-12 12:25:00 -0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import Note from './Note.js'
+import ListNotes from './ListNotes'
+import { Route, Link, Switch } from 'react-router-dom'
+import axios from 'axios'
+
 
 class App extends Component {
+
+
+  constructor (props) {
+    console.log("in the constructor")
+    super(props)
+    this.state = {
+     notes: null
+    }
+
+  }
+
+  componentDidMount () {
+    console.log("did mount")
+    axios.get('/api/notes')
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          notes: res
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
+    console.log("in the render")
     return (
       <div className="App">
-        < Note />
+
+        <main>
+          < Switch>
+            <Route path='/:id' />
+            <Route path='/' render={(routerProps)=>< ListNotes notes={this.state.notes} {...routerProps} {...this.state}/>}/>
+          </Switch>
+        </main>
       </div>
     );
   }
 }
 
 export default App;
+
+
